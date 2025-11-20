83bbbabab2bb3af373072f7588be5021d5b61a2c
BowWarrior <ajf4500@gmail.com>
2025-11-20 00:09:09 -0600

    added solutions to questions 1, 2, and 3

--- firstCPP.cpp
+++ firstCPP.cpp
@@ -1,7 +1,79 @@
+#include <string>
 #include <iostream>
 using namespace std;
 
+void Question1();
+void Question2();
+void Question3();
+
 int main() {
-    cout << "hi";
-    cout << "nuh uhh";
+    //Question1();
+    //Question2();
+    Question3();
+
+
+    return 0;
+}
+
+void Question1(){
+    int x = 7;
+    //returns 1 (true)
+    cout << (x < 10 && x >= -1) << endl;
+    //returns 0 (false)
+    cout << (!x && x >= 3) << endl;
+    //returns 0 (false)
+    cout << (x++ == 8 || x == 7) << endl;
+
+}
+
+void Question2(){
+    //returns 'f'
+    char s[] = "University of Wisconsin-Eau Claire";
+    cout << s[12] << endl;
+
+    //returns "It's been a very sunny day"
+    string example("It's been a very busy day");
+    cout << (example.replace(17, 4, "sunny")) << endl;
+
+    //returns "Angry Birds"
+    string animal("Birds");
+    cout << (animal.insert(0, "Angry ")) << endl;
+
+    //returns "Madness"
+    string game("March Madness");
+    cout << (game.substr(6, 7)) << endl;
+}
+
+
+
+void Question3(){
+    //vectors are apparently used nowadays, but here are normal pointers
+    int size;
+
+    cout << "enter integer amount of how many doubles you want in the array!" << endl;
+    cin >> size;
+
+
+    double** d_array = new double*[size];
+    for(int i = 0; i < size; i++){
+        d_array[i] = new double;
+        
+        cout << "enter new double: " << endl;
+        cin >> *(d_array[i]);
+    }
+
+
+    for(int i = 0; i < size; i++){
+        cout << "value at d_array[" << i << "] = " << *(d_array[i]) << " (at memory address: " << d_array[i] << ")" << endl;
+    }
+    cout << endl;
+
+
+
+    //free memory
+    for (int i = 0; i < size; i++) {
+        delete d_array[i];
+    }
+    delete[] d_array;
+    
 }
